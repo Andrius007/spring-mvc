58e311531c01d269f3f9dae69643877d1ff5186f
Andrius007 <79835280+Andrius007@users.noreply.github.com>
2021-03-26 19:42:00 +0300

    Решение на проверку

---
+++ 2.2.2. spring_mvc/src/main/java/service/Service.java
@@ -0,0 +1,30 @@
+package service;
+
+import Model.Car;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class Service {
+    private static List<Car> carsList;
+
+    public static List<Car> createCars() {
+        carsList = new ArrayList<>();
+        carsList.add(new Car(1, "black", 50));
+        carsList.add(new Car(2, "white", 40));
+        carsList.add(new Car(3, "green", 30));
+        carsList.add(new Car(4, "blue", 20));
+        carsList.add(new Car(5, "red", 10));
+
+
+        return carsList;
+    }
+
+    public static List<Car> selectCars(Integer ammount) {
+
+        createCars();
+        return ((ammount != null) && (ammount <= carsList.size() - 1)) ? carsList.subList(0, ammount) : carsList;
+    }
+}
