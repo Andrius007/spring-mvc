3451bdd4c865a0b57ee20bd91c3b0128f2497186
Andrius007 <79835280+Andrius007@users.noreply.github.com>
2021-04-01 12:38:47 +0300

    Корректировка

---
+++ 2.2.2. spring_mvc/src/main/java/web/Model/Car.java
@@ -0,0 +1,26 @@
+package web.Model;
+
+public class Car {
+    private String model;
+    private String color;
+    private int yearOfBuilt;
+
+    public Car(String model, String color, int yearOfBuilt) {
+        this.model = model;
+        this.color = color;
+        this.yearOfBuilt = yearOfBuilt;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getYearOfBuilt() {
+        return yearOfBuilt;
+    }
+
+}
